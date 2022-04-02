ed23070ae99c906b46ad69ce80bde17b21c81a00
watersing <watersheji@163.com>
2022-04-02 17:29:33 +0800

    第三次提交 加 b c 功能

--- HelloClass.java
+++ HelloClass.java
@@ -2,5 +2,7 @@ public class HelloClass{
     public  static void main (String[] args){
         System.out.println("Hello git");
 		    System.out.println("Hello git:更新完成A功能");
+			em.out.println("Hello git");
+		    System.out.println("Hello git:更新完成B功能");
     }
 }
