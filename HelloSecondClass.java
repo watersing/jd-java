7ab3532f37e179cc8125166ce7391183f8312b90
watersing <watersheji@163.com>
2022-04-02 17:41:14 +0800

    第四次提交删除

--- HelloSecondClass.java
+++
@@ -1,7 +0,0 @@
-public class HelloSecondClass{
-    public  static void main (String[] args){
-        System.out.println("Hello Second");
-		    System.out.println("Hello Second:A功能");
-			System.out.println("Hello Second:C功能");
-    }
-}
